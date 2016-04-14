f3ba56279458eb616b9e21fb1e778c4e2dae53d1
pbhatu <bhatnagar.parth18@gmail.com>
2016-04-14 13:33:25 +0530

    Added files via upload

---
+++ medicine.php
@@ -0,0 +1,33 @@
+<html>
+<head><link rel="stylesheet" type="text/css" href="medicine-style.css">
+</head>
+<body id="body-color">
+<div id="dis">
+<fieldset style="width:30%"><legend>Search Generic Drug</legend>
+<table border="0">
+<!--<tr> -->
+<form method="POST" action="Drug_Med.php"> 
+<!--td>Name</td>
+<td> 
+<input type="text" name="name"></td> 
+</tr>
+<tr>-->
+<td>Enter Branded Drug</td><td> <input type="text" name="B_Drug"></td>
+</tr>
+<!--<tr>
+<td>Enter Symptoms</td><td> <input type="text" name=Symptoms></td>
+</tr>
+<tr>
+<td> Enter Symptoms </td><td><textarea rows="5" cols="60" name="message"></textarea></td>
+</tr>--> 
+<div class="button">
+<tr>
+<td><input type="submit" name="submit" value="Submit"></td>
+
+</tr>
+</form>
+</table>
+</fieldset>
+</div>
+</body>
+</html>
