99c11644b34b56488d428622417a7a6be7db71b5
pbhatu <bhatnagar.parth18@gmail.com>
2016-04-14 13:24:55 +0530

    Create Home.php
    Home page of web application

---
+++ Home.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<div class="menu-wrap">
+    <nav class="menu">
+        <ul class="clearfix">
+            <li><a href="home.php">Home</a></li>
+            <li>
+                <a href="#">Testimonials <span class="arrow">&#9660;</span></a>
+                <ul class="sub-menu">
+                    <li><a href="signup.php">Post a Testimonial </a></li>
+                    <li><a href="display_testimonials.php">Display Testimonial</a></li>
+                    
+                </ul>
+            </li>
+            <li><a href="#">Find a Generic Drug <span class="arrow">&#9660;</span></a>
+            <ul class="sub-menu">
+                <li><a href="disease.php">Find by Disease</a>
+                  <ul class="sub-menu1">
+                     <!--<li><a href="#"> Heart Diseases </a></li>
+                     <li><a href="#"> Digestive System </a></li>-->
+                  </ul>
+                  </li>
+                <li><a href="medicine.php">Find by medicine</a></li>
+                </ul></li>
+            <li ><a href="#">Contact</a></li>
+            <li><a href="#">Site Help</a></li>
+        </ul>
+    </nav>
+</div>
