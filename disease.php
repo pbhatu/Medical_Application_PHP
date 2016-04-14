8a1c23402d6899db6aa8a400703aa9dedae2f37d
pbhatu <bhatnagar.parth18@gmail.com>
2016-04-14 13:35:14 +0530

    Added files via upload

---
+++ disease.php
@@ -0,0 +1,34 @@
+<html>
+<head><link rel="stylesheet" type="text/css" href="disease-style.css">
+<title>Sign-up</title>
+</head>
+<body id="body-color">
+<div id="dis">
+<fieldset style="width:30%"><legend>Search Generic Drug</legend>
+<table border="0">
+<!--<tr> -->
+<form method="POST" action="Drug.php"> 
+<!--td>Name</td>
+<td> 
+<input type="text" name="name"></td> 
+</tr>
+<tr>-->
+<td>Enter Disease</td><td> <input type="text" name="Disease"></td>
+</tr>
+<!--<tr>
+<td>Enter Symptoms</td><td> <input type="text" name=Symptoms></td>
+</tr>
+<tr>
+<td> Enter Symptoms </td><td><textarea rows="5" cols="60" name="message"></textarea></td>
+</tr>--> 
+<div class="button">
+<tr>
+<td><input type="submit" name="submit" value="Submit"></td>
+
+</tr>
+</form>
+</table>
+</fieldset>
+</div>
+</body>
+</html>
