2d7761ac6c92134684211c844b2b8a5689b56c94
pbhatu <bhatnagar.parth18@gmail.com>
2016-04-14 13:27:46 +0530

    Create testimonials_form.php
    Signup form for adding testimonials.

---
+++ testimonials_form.php
@@ -0,0 +1,39 @@
+
+
+<html>
+<head><link rel="stylesheet" type="text/css" href="signup-style.css">
+<title>Sign-up</title>
+</head>
+<body id="body-color">
+<div id="sign-up">
+<fieldset style="width:30%"><legend>Testimonial Form</legend>
+<table border="0">
+<tr> 
+<form method="POST" action="testimonials.php"> 
+<td>Name</td>
+<td> 
+<input type="text" name="name"></td> 
+</tr>
+<tr>
+<td>Email</td><td> <input type="text" name="email"></td>
+</tr>
+<tr>
+<td>Disease</td><td> <input type="text" name="Dis"></td>
+</tr>
+<tr>
+<td> Enter Message </td><td><textarea rows="5" cols="60" name="message"></textarea></td>
+</tr>
+<div class="button">
+<tr>
+<td><input type="submit" name="submit" value="Submit"></td>
+
+</tr>
+</form>
+</table>
+</fieldset>
+</div>
+</body>
+</html>
+
+
+
